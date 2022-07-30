2cd8a8b6ffdc6ab6aaed0fb8f4a581f523e8af25
MinhNamThan <thanminhnam17022001@gmail.com>
2022-07-30 18:03:51 +0700

    add layout

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,43 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "@yaireo/tagify";
+import "appear";
+import "bootstrap";
+import "chart.js";
+import "chartjs-chart-matrix";
+import "chartjs-plugin-datalabels";
+import "circles.js";
+import "clipboard";
+import "datatables";
+import "datatables.net-buttons";
+import "daterangepicker";
+import "daterangepicker/daterangepicker.css";
+import "dropzone";
+import "flag-icon-css/css/flag-icon.min.css"
+import "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import "ion-rangeslider";
+import "jquery-mask-plugin";
+import "jquery-migrate";
+import "jquery-validation";
+import "jszip";
+import "leaflet";
+import "list.js";
+import "pdfmake";
+import "pwstrength-bootstrap/dist/pwstrength-bootstrap.min";
+import "quill";
+import "select2";
+import "select2/dist/css/select2.min.css";
+import "sortablejs";
